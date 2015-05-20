85a0c19e828b2a85f0be9f3d9af2ca77806024dd
WagnerLab <dr.bagels@gmail.com>
2015-05-20 01:55:40 -0400

    adjusted tutorial roi script and added csv files for roi analysis

--- tutorial_data/scripts/roi_tutorial.m
+++ tutorial_data/scripts/roi_tutorial.m
@@ -1,6 +1,6 @@
 % spm12w r6225
 % Parameters file for roi analysis
-% Last updated: April, 2015
+% Last updated: May, 2015
 % =======1=========2=========3=========4=========5=========6=========7=========8
 
 % User name
@@ -11,26 +11,28 @@ roi.study_dir = 'C:\Users\ddw\Documents\MATLAB\tutorial_data';
 roi.glm_name = 'glm_tutorial';      % glm to use for extracting parameters
 roi.roi_name = 'roi_tutorial';      % name of directory for roi analysis output
 
-% Files specifying roi specifications and subject variables (optional)
-roi.spec_file = 'roi_tutorial.csv'; % name of a csv file specifying rois
-roi.var_file = 'roi_tutorial.csv';  % name of a csv file with subject variables
+% CSV files specifying roi specs and subject variables (optional)
+roi.spec_file = 'tutorial_spec.csv'; % name of a csv file specifying rois
+roi.var_file = 'tutorial_vars.csv';  % name of a csv file with subject variables
 
 % 1st level GLM contrasts to use for roi analysis 
 roi.conds = {'allVSbaseline','humVSall'};
 
-% ROI specifications - (mni coordinates, sphere diameter in mm)
-%                    - for masks use nifti image name instead of coordiante
-roi.roi.dmpfc_ba10 = {[-3,63,18],6};
-roi.roi.precuneus_ba32 = {[3,-60,24],6};
-roi.roi.amygdala = {'aal_l_amyg_3x3x3.nii'};
+% ROI specifications - (X,Y,Z, sphere diameter in mm) or mask filename
+%                    - for masks use nifti image name instead of coordinate
+%                    - if no sphere diameter specified, default will be used
+%                    - can be used in conjunction with an roi spec file. 
+roi.roi.l_dmpfc_ba10 = [-3,63,15,6];
+roi.roi.l_precuneus_ba32 = [-3,-60,24];
+roi.roi.l_amygdala = 'aal_l_amyg_3x3x3.nii';
 
 % ROI statistics - statistics: descriptives, ttest1 ttest2, correl1, correl2
-%                - leave blank if statistic not desired
+%                - leave blank or omit if statistic not desired
 %                - use cell arrays for stats on multiple contrasts
-%                - use strings to define formula
+%                - use strings to define a contrast formula
 %                - 'all_conditions' is a reserved word
 roi.stats.descriptives = 'all_conditions';
 roi.stats.ttest1 = {'allVSbaseline','humVSall vs. allVSbaseline'};
-roi.stats.ttest2 = {'allvsbaseline, humVSall'};
+roi.stats.ttest2 = {'allVSbaseline', 'allVSbaseline humVSall'};
 roi.stats.correl1 = 'humVSall';
 roi.stats.correl2 = 'allVSbaseline';
